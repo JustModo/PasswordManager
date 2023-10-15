f95899f68d4770df98b627c7250479f1056da894
Modo <yashlaxman@gmail.com>
2023-10-15 18:41:27 +0530

    Add api request changes

--- frontend/index.js
+++ frontend/index.js
@@ -38,32 +38,71 @@ function validateLogin() {
 async function getAuth() {
 let _username = document.getElementById("username").value;
 let _password = document.getElementById("password").value;
+
+formData = new FormData();
+formData.append('user_name', _username);
+formData.append('password', _password);
+console.log(formData)
+
     try {
+<<<<<<< HEAD
         const response = await fetch('http://127.0.0.1:5000/', {
             method: 'POST',
             headers: {
+=======
+        const response = await fetch('/login', {
+            method: "post",
+            /*headers: {
+>>>>>>> b1b896f (Add api request changes)
                 'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                user_name: _username,
-                password: _password
-            })
+            }*/
+            body: formData
         });
         
-        if(!response.ok) {
-            throw new Error('Connection was not Ok!');
+        if(response.ok) {
+            //next page
+        } else if(!response.ok) {
+            if (response.status === 403) {
+                const error = new Error('Access denied: You do not have permission to access this resource.');
+                    document.getElementById("errlabel").innerHTML = "Invalid Details!";
+                setTimeout(()=> {
+                    document.getElementById("errlabel").innerHTML = "";
+                },3000)
+                throw error;
+            }
         }
 
-        const data = await response.json();
-        const token = data.token;
+        //error
+        /*const errorData = await response.json();
+        if (response.status === 401) {
+            throw new Error('Invalid username or password')
+        } else {
+            throw new Error(errorData.error)    
+        }*/
+        
+        // const data = await response.json();
+        // const token = data.token;
+        // console.log(`Got token: ${token}`)
+        // console.log(data)
+        // handleAuthRes(data);
 
+
+<<<<<<< HEAD
         console.log(`Got token: ${token}`)
     } catch (error) {
         console.error('Error:', error.message);
+=======
+    } catch (error) {
+        console.log(error);
+        if(error instanceof TypeError && error.message === 'Failed to fetch'){
+            console.log("Failed Server")
+        }
+>>>>>>> b1b896f (Add api request changes)
     }
         
 }
 
+<<<<<<< HEAD
 function handleAuthRes(data) {
     if(data.token){
         localStorage.setItem('token', token.data);
@@ -72,3 +111,13 @@ function handleAuthRes(data) {
         console.error('Login Failed:', data.error)
     }
 }
+=======
+// function handleAuthRes(data) {
+//     if(data.token){
+//         localStorage.setItem('token', data.token);
+//         console.log("Logged In...")
+//     } else {
+//         console.error('Login Failed:', data.error)
+//     }
+// }
+>>>>>>> b1b896f (Add api request changes)
