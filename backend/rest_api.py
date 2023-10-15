81921fa4ba288f1ec2bcd99d5da73af5cf78cca6
Kosh <koshinhegde@gmail.com>
2023-10-15 18:54:06 +0530

    Interacting with data with rest api

--- backend/rest_api.py
+++ backend/rest_api.py
@@ -1,8 +1,9 @@
 from os import mkdir, path
+import json
 from flask import Flask, request, send_file, abort, Response
 from werkzeug.datastructures import ImmutableMultiDict
 import appdirs
-import json
+
 from data_handler import DataHandler
 
 
@@ -74,6 +75,21 @@ class QueryHandler:
         return Response(status=200)
 
     @staticmethod
+    @app.post("/logout")
+    def logout() -> Response:
+        """
+        Logs the user out
+        """
+        if QueryHandler.__data_handler is None:
+            return Response(
+                    "Not logged in",
+                    403,
+                    content_type="text/plain"
+            )
+        QueryHandler.__data_handler = None
+        return Response(status=200)
+
+    @staticmethod
     @app.post("/add_user")
     def add_user() -> Response:
         """
@@ -119,8 +135,324 @@ class QueryHandler:
         return Response(
                 json.dumps(QueryHandler.__data_handler.get_data()),
                 200,
-                content_type="application/json"
+                content_type="text/json"
         )
+
+    @staticmethod
+    @app.post("/change_password")
+    def change_password() -> Response:
+        """
+        Change the password of the user.
+        """
+        if QueryHandler.__data_handler is None:
+            return Response(
+                    "Not logged in",
+                    403,
+                    content_type="text/plain"
+                    )
+
+        data: ImmutableMultiDict[str, str] = request.form
+        if "password" not in data:
+            return Response(
+                    "password is not given",
+                    400,
+                    content_type="text/plain"
+            )
+
+        QueryHandler.__data_handler.change_password(data["password"])
+
+        return Response(status=200)
+
+    @staticmethod
+    @app.post("/add_entry")
+    def add_entry() -> Response:
+        """
+        Add a new entry to the data
+        """
+        if QueryHandler.__data_handler is None:
+            return Response(
+                    "Not logged in",
+                    403,
+                    content_type="text/plain"
+                    )
+
+        data: ImmutableMultiDict[str, str] = request.form
+        if "entry_name" not in data:
+            return Response(
+                    "entry_name is not given",
+                    400,
+                    content_type="text/plain"
+            )
+        if "fields" not in data:
+            return Response(
+                    "fields is not given",
+                    400,
+                    content_type="text/plain"
+            )
+        entry_name: str = data["entry_name"]
+        try:
+            fields: dict = json.loads(data["fields"])
+        except json.decoder.JSONDecodeError:
+            return Response(
+                    f"The json-string provided is wrong {data['fields']}",
+                    400,
+                    content_type="text/plain"
+            )
+        try:
+            QueryHandler.__data_handler.add_entry(entry_name, fields)
+        except ValueError as value_error:
+            return Response(
+                    str(value_error),
+                    403,
+                    content_type="text/plain"
+            )
+        return Response(status=200)
+
+    @staticmethod
+    @app.post("/delete_entry")
+    def delete_entry() -> Response:
+        """
+        Delete Entry for the logged in user
+        """
+        if QueryHandler.__data_handler is None:
+            return Response(
+                    "Not logged in",
+                    403,
+                    content_type="text/plain"
+            )
+
+        data: ImmutableMultiDict[str, str] = request.form
+        if "entry_name" not in data:
+            return Response(
+                    "entry_name is not given",
+                    400,
+                    content_type="text/plain"
+            )
+        entry_name: str = data["entry_name"]
+        try:
+            QueryHandler.__data_handler.delete_entry(entry_name)
+        except ValueError as value_error:
+            return Response(
+                    str(value_error),
+                    403,
+                    content_type="text/plain"
+            )
+        return Response(status=200)
+
+    @staticmethod
+    @app.post("/edit_entry_name")
+    def edit_entry_name() -> Response:
+        """
+        Delete Entry for the logged in user
+        """
+        if QueryHandler.__data_handler is None:
+            return Response(
+                    "Not logged in",
+                    403,
+                    content_type="text/plain"
+            )
+
+        data: ImmutableMultiDict[str, str] = request.form
+        if "old_entry_name" not in data:
+            return Response(
+                    "old_entry_name is not given",
+                    400,
+                    content_type="text/plain"
+            )
+        if "new_entry_name" not in data:
+            return Response(
+                    "new_entry_name is not given",
+                    400,
+                    content_type="text/plain"
+            )
+        try:
+            QueryHandler.__data_handler.edit_entry_name(
+                    data["old_entry_name"], data["new_entry_name"]
+            )
+        except ValueError as value_error:
+            return Response(
+                    str(value_error),
+                    403,
+                    content_type="text/plain"
+            )
+        return Response(status=200)
+
+    @staticmethod
+    @app.post("/add_field")
+    def add_field() -> Response:
+        """
+        Add a field to the entry
+        """
+        if QueryHandler.__data_handler is None:
+            return Response(
+                    "Not logged in",
+                    403,
+                    content_type="text/plain"
+            )
+
+        data: ImmutableMultiDict[str, str] = request.form
+        if "entry_name" not in data:
+            return Response(
+                    "entry_name is not given",
+                    400,
+                    content_type="text/plain"
+            )
+        if "field_name" not in data:
+            return Response(
+                    "field_name is not given",
+                    400,
+                    content_type="text/plain"
+            )
+        if "field_value" not in data:
+            return Response(
+                    "field_value is not given",
+                    400,
+                    content_type="text/plain"
+            )
+        try:
+            QueryHandler.__data_handler.add_field(
+                    data["entry_name"],
+                    data["field_name"],
+                    data["field_value"]
+            )
+        except ValueError as value_error:
+            return Response(
+                    str(value_error),
+                    403,
+                    content_type="text/plain"
+            )
+        return Response(status=200)
+
+    @staticmethod
+    @app.post("/delete_field")
+    def delete_field() -> Response:
+        """
+        Delete a field from a entry
+        """
+        if QueryHandler.__data_handler is None:
+            return Response(
+                    "Not logged in",
+                    403,
+                    content_type="text/plain"
+            )
+
+        data: ImmutableMultiDict[str, str] = request.form
+        if "entry_name" not in data:
+            return Response(
+                    "entry_name is not given",
+                    400,
+                    content_type="text/plain"
+            )
+        if "field_name" not in data:
+            return Response(
+                    "field_name is not given",
+                    400,
+                    content_type="text/plain"
+            )
+        try:
+            QueryHandler.__data_handler.delete_field(
+                    data["entry_name"],
+                    data["field_name"]
+            )
+        except ValueError as value_error:
+            return Response(
+                    str(value_error),
+                    403,
+                    content_type="text/plain"
+            )
+        return Response(status=200)
+
+    @staticmethod
+    @app.post("/edit_field_name")
+    def edit_field_name() -> Response:
+        """
+        Change the field name in a entry
+        """
+        if QueryHandler.__data_handler is None:
+            return Response(
+                    "Not logged in",
+                    403,
+                    content_type="text/plain"
+            )
+
+        data: ImmutableMultiDict[str, str] = request.form
+        if "entry_name" not in data:
+            return Response(
+                    "entry_name is not given",
+                    400,
+                    content_type="text/plain"
+            )
+        if "old_field_name" not in data:
+            return Response(
+                    "old_field_name is not given",
+                    400,
+                    content_type="text/plain"
+            )
+        if "new_field_name" not in data:
+            return Response(
+                    "new_field_name is not given",
+                    400,
+                    content_type="text/plain"
+            )
+        try:
+            QueryHandler.__data_handler.edit_field_name(
+                    data["entry_name"],
+                    data["old_field_name"],
+                    data["new_field_name"]
+            )
+        except ValueError as value_error:
+            return Response(
+                    str(value_error),
+                    403,
+                    content_type="text/plain"
+            )
+        return Response(status=200)
+
+    @staticmethod
+    @app.post("/edit_field_value")
+    def edit_field_value() -> Response:
+        """
+        Change the value of a field
+        """
+        if QueryHandler.__data_handler is None:
+            return Response(
+                    "Not logged in",
+                    403,
+                    content_type="text/plain"
+            )
+
+        data: ImmutableMultiDict[str, str] = request.form
+        if "entry_name" not in data:
+            return Response(
+                    "entry_name is not given",
+                    400,
+                    content_type="text/plain"
+            )
+        if "field_name" not in data:
+            return Response(
+                    "field_name is not given",
+                    400,
+                    content_type="text/plain"
+            )
+        if "field_value" not in data:
+            return Response(
+                    "field_value is not given",
+                    400,
+                    content_type="text/plain"
+            )
+        try:
+            QueryHandler.__data_handler.edit_field_value(
+                    data["entry_name"],
+                    data["field_name"],
+                    data["field_value"]
+            )
+        except ValueError as value_error:
+            return Response(
+                    str(value_error),
+                    403,
+                    content_type="text/plain"
+            )
+        return Response(status=200)
 
 import os
 from os import path
