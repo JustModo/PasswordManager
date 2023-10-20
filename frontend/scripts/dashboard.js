464820aacfcab57b5ebb37c615725ddc0a3834e2
Modo <yashlaxman@gmail.com>
2023-10-20 22:13:41 +0530

    Add Entry List with Functionality

--- frontend/scripts/dashboard.js
+++ frontend/scripts/dashboard.js
@@ -1,16 +1,6 @@
 
-const search = document.getElementById('search');
+//-----------------------------------------------------------------------------------------(Getting Data)
 let globaldata;
-
-search.addEventListener('keyup', function() {
-    let result = search.value;
-    if(result == ""){
-        updatelist(globaldata)
-    } else {
-        console.log(result);
-        updatelist(globaldata)
-    }
-});
 
 async function getData() {
         try {
@@ -23,9 +13,9 @@ async function getData() {
             
             if(response.ok) {
                 let userdata = await response.json();
-                console.log(userdata)
+                //console.log(userdata)
                 globaldata = userdata
-                updatelist(userdata);
+                getFieldName(userdata)
             } else if(!response.ok) {
                 const errorMessage = await response.text();
                 throw new Error(errorMessage);
@@ -35,39 +25,109 @@ async function getData() {
         }
     }
 
-
-// function addlist(userdata) {
-//     let masterdata = [];
-//     for (let field in userdata){
-//         masterdata.push(userdata[field])
-//     }
-//     console.log(...masterdata)
-//     updatelist(masterdata)
-// }
+//-----------------------------------------------------------------------------------------(Adds Values to Content Box)
 
 const contentbox = document.querySelector('.objcontainer')
 
-function updatelist(userdata) {
-        contentbox.innerHTML = ''
-        for(let field in userdata){
-            let fields = userdata[field];
-            for (let data in fields) {
-                if (fields.hasOwnProperty(data)) {
-                    let value = fields[data];
-                    updategui(field, data, value)
-                    console.log(field, data, value)
-                }
-        
-            }
-        }
-            
+function updateListGui(field) {
+        let content = contentbox.innerHTML.concat(
+            `
+            <div>
+            <img src="${field["url"]}">
+            <p>${field["fieldname"]}</p>
+             <button id="${field["fieldname"]}" class="entrybutton"> Show </button>
+            </div>
+            `
+            )
+        contentbox.innerHTML = content; 
+
+        let btns = document.querySelectorAll('.entrybutton')
+        btns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                updateInfoGui(btn.id);
+            });
+        });      
 }
 
-function updategui(title, key, value) {
-    console.log(`${title},${key},${value}`)
-    let content = contentbox.innerHTML.concat(`<div>${title},${key},${value}</div>`)
-    contentbox.innerHTML = content;
+
+// const data = {
+//     Amazon: {
+//         url: "https://www.amazon.com",
+//         Name: "Yash",
+//         Password: "123"
+//     },
+//     Google: {
+//         url: "https://www.google.com",
+//         Name: "XYZ",
+//         Password: "Secret"
+//     }
+// }
+
+function getFieldName(data) {                       //Gets All Field Values
+  let fieldnamearray = []
+  for(let field in data){
+    let fieldname = field
+    fieldnamearray.push(fieldname)
+  }
+  getDisplayList(fieldnamearray,data);
+  //console.log(fieldnamearray)
 }
+
+function getFaviconUrl(fieldname,data) {
+  let fielddata = data[fieldname]
+  let url = fielddata["url"]
+  url = url.replace("https://",'')
+  faviconlink = `http://www.google.com/s2/favicons?domain=${url}`
+  return faviconlink
+}
+
+function getDisplayList(fieldnames,userdata){
+  contentbox.innerHTML = ''
+  for(let i in fieldnames){
+    let faviconurl = getFaviconUrl(fieldnames[i],userdata)
+    let guidata = {
+      fieldname: fieldnames[i],
+      url: faviconurl
+    }
+    updateListGui(guidata)
+  }
+}
+
+//-----------------------------------------------------------------------------------------
+
+const search = document.getElementById('search');
+
+search.addEventListener('keyup', function() {
+    let result = search.value;
+    if(result == ""){
+        getData()
+    } else {
+        getFilterList(result,globaldata)
+        //console.log(result);
+        
+    }
+});
+
+function getFilterList(keyword,data) {                       //Gets Filtered Field Values
+    let filterarray = []
+    for(let field in data){
+        if(field.toLowerCase().indexOf(keyword) !== -1){
+            let fieldname = field
+            filterarray.push(fieldname)
+        }
+    }
+    getDisplayList(filterarray,data);
+    //console.log(filterarray)
+  }
+
+//-----------------------------------------------------------------------------------------
+
+
+function updateInfoGui(call) {
+    console.log(call)
+}
+
+//-----------------------------------------------------------------------------------------
 
 window.onload = function() {
     getData();
